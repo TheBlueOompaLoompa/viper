e176fd6591c0e3bf19436cee7bd1196b1ef2b7ef
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-29 12:58:07 -0500

    Add on:click event to WideButton

--- src/components/WideButton.svelte
+++ src/components/WideButton.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { createEventDispatcher } from 'svelte';
 	export let text;
 	export let style = '';
+	let dispatch = createEventDispatcher();
+
+	function onClick() {
+		dispatch("click");
+	}
 </script>
-<div class="button w-wide" style={style}><span class="gray">{text}</span></div>
+<div class="button w-wide" style={style} on:click={onClick}><span class="gray">{text}</span></div>
