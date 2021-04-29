df970f7ac19bfd19d6b77b8bad7a6109172097b0
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-29 00:51:02 -0500

    Create sign in/up selection page

---
+++ src/routes/sign.svelte
@@ -0,0 +1,48 @@
+<script>
+	import WideButton from "../components/WideButton.svelte";
+</script>
+
+<div class="center" style="flex-direction: column;">
+	<h1>VIPER</h1>
+	<p>Viper is a privacy focused social media app for everyone</p>
+
+	<div id="button-container">
+		<WideButton text="Register" style="margin-bottom: 40px;"></WideButton>
+		<WideButton text="Login"></WideButton>
+	</div>
+</div>
+
+<style>
+	@media (max-width: 800px) {
+		#button-container {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: end;
+
+			max-width: 400px;
+			width: 100%;
+
+			position: fixed;
+
+			bottom: 35px;
+		}
+	}
+
+	@media (min-width: 800px) {
+		#button-container {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+
+			max-width: 400px;
+			width: 100%;
+			bottom: 0px;
+
+			height: 100%;
+
+			position: absolute;
+		}
+	}
+</style>
