70c8d0184a4ccabf148fe3debe8f9e40cf1bc4b1
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-29 12:57:23 -0500

    Setup placeholder page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
-<script>
-	import WideButton from "../components/WideButton.svelte";
-
-	window.location.href = "sign";
+<script lang="typescript">
+	import WideButton from "../components/WideButton.svelte"
 </script>
 
-<div class="center">
-	<WideButton text="Test Button"></WideButton>
-</div>
+<WideButton text="Placeholder"></WideButton>
