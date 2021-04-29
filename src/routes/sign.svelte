e8f5df916eff27c96262a0a6b6efdf40ac6c0f04
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-29 11:39:19 -0500

    Move css to a global css class

--- src/routes/sign.svelte
+++ src/routes/sign.svelte
@@ -6,43 +6,8 @@
 	<h1>VIPER</h1>
 	<p>Viper is a privacy focused social media app for everyone</p>
 
-	<div id="button-container">
+	<div class=".center-control">
 		<WideButton text="Register" style="margin-bottom: 40px;"></WideButton>
 		<WideButton text="Login"></WideButton>
 	</div>
 </div>
-
-<style>
-	@media (max-width: 800px) {
-		#button-container {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: end;
-
-			max-width: 400px;
-			width: 100%;
-
-			position: fixed;
-
-			bottom: 35px;
-		}
-	}
-
-	@media (min-width: 800px) {
-		#button-container {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: center;
-
-			max-width: 400px;
-			width: 100%;
-			bottom: 0px;
-
-			height: 100%;
-
-			position: absolute;
-		}
-	}
-</style>
