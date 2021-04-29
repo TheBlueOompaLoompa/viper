fde4b94d4ad85f06b8a6167ec5d0d923cd8fb7e8
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-29 12:58:31 -0500

    Create layout.svelte and add auth check

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	import { supabase } from '../supa';
+
+	onMount(() => {
+		if(!supabase.auth.session() && window.location.href != '/sign')
+			window.location.href = '/sign';
+	});
+</script>
+<slot></slot>
