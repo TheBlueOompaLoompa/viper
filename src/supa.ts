0c23e57e57cd051df82440df02ecb1a087a4d833
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-05-01 22:04:06 -0500

    Change location of supabase lib

--- src/supa.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTcxMjQ1NCwiZXhwIjoxOTM1Mjg4NDU0fQ.fAmYR_p8yRSvfdtbEc7UwAXKTlc5r2Jw4pW2MrXqpi8';
-const SUPABASE_URL = "https://lzpombjaaizuqwdpggzf.supabase.co";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
