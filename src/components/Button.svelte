a8e7bc6a3e343a083ae9526dc5e924440b100877
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-05-04 16:56:52 -0500

    new(Create button) This will replace all wide buttons soon

---
+++ src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	export let text;
+	export let style = '';
+	export let wide = false;
+	let dispatch = createEventDispatcher();
+
+	function onClick() {
+		dispatch('click');
+	}
+</script>
+
+<div class="button {wide ? 'w-wide' : ''}" style={style} on:click={onClick}><span class="gray">{text}</span></div>
