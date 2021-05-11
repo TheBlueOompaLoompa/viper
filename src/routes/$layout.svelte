702235b6d95a95e6febcce290a1e6e6272ed3184
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-05-11 18:44:56 -0500

    add(message bar to ask for signup/in) No more redirects

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
+	import Navbar from '../components/Navbar.svelte';
+	import Button from '../components/Button.svelte';
+
 	import { onMount } from 'svelte';
 
 	import supabase from '$lib/db';
 
-	import Navbar from '../components/Navbar.svelte';
 
 	let showNav = false;
+	let showLogMsg = false;
 	let page = 'home';
+
+	function gotoSign() {
+		window.location.href = '/sign';
+	}
 
 	onMount(() => {
 		showNav = false;
 
 		if (!supabase.auth.user() && !window.location.href.includes('sign'))
-			window.location.href = '/sign';
+			showLogMsg = true;
 
 		setInterval(() => {
 			if (!window.location.href.includes('sign') && !window.location.href.includes('setup'))
@@ -43,8 +50,43 @@
 	<Navbar {page} />
 {/if}
 
+{#if showLogMsg}
+	<p id="close" on:click={() => { showLogMsg = !showLogMsg; }}>X</p>
+	<div class="logmsg center" style="flex-direction: column;">
+		<p>You aren't currently logged in.</p>
+		<br>
+		<Button text="Signup or Login" wide={true} on:click={gotoSign} />
+	</div>
+{/if}
+
 <style>
 	main {
 		padding-bottom: 42px;
+	}
+
+	.logmsg {
+		position: fixed;
+		bottom: 0px;
+		left: 0px;
+		right: 0px;
+		height: 200px;
+
+		background-color: rgb(13, 13, 17);
+		box-shadow: 0px -4px 4px var(--theme-color-accent-mid);
+
+		z-index: 2;
+	}
+
+	#close {
+		position: fixed;
+
+		right: 15px;
+		bottom: 150px;
+
+		z-index: 3;
+
+		user-select: none;
+		-moz-user-select: none;
+		-webkit-user-select: none;
 	}
 </style>
