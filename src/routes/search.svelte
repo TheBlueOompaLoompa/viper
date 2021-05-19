9ad06520c75d0e23397e9f120b106c0752b982dc
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-05-18 23:58:43 -0500

    add(post display) fix(move post fetching into rpc call) add(scroll to load)

--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -1,33 +1,55 @@
 <script lang="typescript">
 	import supabase from '$lib/db';
 	import vfetch from '$lib/vfetch';
+
+	import { fly } from 'svelte/transition';
+	import Post from '../components/Post.svelte';
 	
 	let search = "";
 	
 	let results = [];
 	
-	async function onUpdateSearch(srch) {
-		alert('Update search');
+	function thing() {
+		window.onscroll = async function () {
+			if (
+				window.innerHeight + window.scrollY >= document.body.scrollHeight &&
+				!scrollLoadDisabled
+			) {
+				scrollLoadDisabled = true;
+				offset += 5;
+
+				results = [
+					...results,
+					...((await supabase.rpc('srch', {term: search.replace(' ', ' or '), req_offset: offset})).data)
+				];
+
+				scrollLoadDisabled = false;
+			}
+		};	
+	}
+
+	let first = true;
+	async function onUpdateSearch() {
+		if(first){
+			first = false;
+			thing();
+		}
 		try {
-			results = await supabase
-				.from('posts')
-				.select('*')
-				.textSearch('name', `'%${srch}%'`, {
-					type: 'websearch',
-					config: 'english'
-				});
+			results = (await supabase.rpc('srch', {term: search.replace(' ', ' or '), req_offset: 0})).data;
 		}catch(e) {
 			alert(e);
 		}
-		
 	}
-	
+
 	$: {
-		console.log(search);
-		onUpdateSearch(search);
+		if(search.replace(' ', '') != ''){
+			onUpdateSearch();
+		}
 	}
-	
-	let posts = [];
+
+	var scrollLoadDisabled = false;
+	var offset = 0;
+
 	let images = {};
 	let usernameCache = {};
 	async function fetchImage(post) {
@@ -40,13 +62,32 @@
 			usernameCache[post['uid']] = await vfetch.getUsernameFromPost(post);
 		}
 	}
-	$: results.forEach(async (post) => {
-		fetchImage(post);
-		cacheUsername(post);
-	});
+
+	var i = 0;
+	$: for(i = 0; i < results.length; i++){
+		fetchImage(results[i]);
+		cacheUsername(results[i]);
+	}
 </script>
 
 <div class="center" style="flex-direction: column;">
 	<h2>Search</h2>
 	<input type="text" class="w-wide" bind:value={search} placeholder="Users, posts, groups, etc..." />
 </div>
+<posts class="center" style="display: flex; flex-direction:column; margin-top: 30px;">
+
+	{#each results as post, i}
+		<div
+			class="trans">
+			<Post {post} cache={usernameCache} img={images[post['id']]} />
+		</div>
+	{/each}
+</posts>
+
+<style>
+	.trans {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
+</style>
