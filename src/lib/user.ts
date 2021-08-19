42829963b8ea29ede83ebdd6a9fe023249e831fc
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-08-19 05:37:41 +0100

    Remove all the things

--- src/lib/user.ts
+++
@@ -1,4 +0,0 @@
-export default interface user {
-	id: string;
-	username: string;
-}
--- src/routes/dm.svelte
+++
@@ -1,4 +0,0 @@
-<p style="margin: 15px">
-	Our coding snakes are hard at work building this feature, please bear with us while we develop
-	this new part of the Viper app.
-</p>
