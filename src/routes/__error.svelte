3b12625d6c98822ea6fe7c4d22f9401e052c29a4
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-08-21 00:31:43 -0500

    add 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	export function load({ error, status }) {
+return {
+	props: {
+		status,
+		error
+	}
+};
+	}
+</script>
+
+<script>
+	export let status;
+	export let error;
+</script>
+
+<center>
+	<h1>Error {status}</h1>
+	<span>{error.message}</span>
+</center>
