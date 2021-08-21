8b48dbdeae38ffe2d5457e9339d2e0c0d5f4bd1e
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-08-21 00:31:20 -0500

    rename layout.svelte to __layout.svelte

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import Navbar from '../components/Navbar.svelte';	
+
+	onMount(() => {
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.cookie = 'theme=dark';
+		}else {
+			document.cookie = 'theme=white';
+		}
+
+		setInterval(() => {
+			if(window.location.pathname == '/profile') {
+				window.location.href = '/login';
+			}
+		}, 30);
+	});
+</script>
+
+<slot></slot>
+
+<Navbar />
