c2711e3266defbf2ca3acb9ac61daba6ce198edd
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-08-24 02:16:14 -0500

    add logout button to settings page

---
+++ src/routes/settings.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let logout;
+
+	onMount(async () => {
+        const { auth } = await import('../firebase');
+
+		logout = function() {
+			auth.signOut();
+		}
+	});
+
+	let group = 'profile';
+</script>
+
+<main>
+	<groups>
+		<button>Profile</button>
+	</groups>
+	<settings>
+		{#if group == 'profile'}
+			<button class="small" on:click={logout}>Logout</button>
+		{/if}
+	</settings>
+</main>
+
+<style>
+	groups,
+	groups * {
+		max-width: 250px;
+	}
+
+	button:not(.small) {
+		width: 250px;
+	}
+
+	main {
+		margin: 20px auto 20px auto;
+		max-width: 600px;
+	}
+</style>
