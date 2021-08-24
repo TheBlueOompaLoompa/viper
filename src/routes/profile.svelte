51fb9097ef04e81c248be12c5b394a3e8b42a089
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-08-24 02:15:56 -0500

    add settings button to profile page

---
+++ src/routes/profile.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Gear from 'svelte-bootstrap-icons/lib/Gear';
+
+	function openSettings() {
+		window.location.href = '/settings';
+	}
+</script>
+
+<div id="settings">
+	<Gear on:click={openSettings} />
+</div>
+
+<style>
+	#settings {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+	}
+</style>
