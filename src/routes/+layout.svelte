29e3d75af12c36e7b7b821bf9d566466ff3fc238
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2023-05-01 23:43:34 -0500

    forgot enable prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    export const prerender = true;
+    
     import Navbar from '$lib/components/Navbar.svelte';
 	import supabase from '$lib/supabase';
 	import { onMount } from 'svelte';
--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+    export const prerender = true;
+    
 	import supabase from "$lib/supabase";
 	import { onMount } from "svelte";
 
